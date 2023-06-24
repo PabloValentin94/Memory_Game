9cbd2d947c1b7ad4468af7df7cdc0cb06795a7de
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-06-24 02:29:51 -0300

    Primeiro commit.
    Criando a estrutura base, fazendo a página do jogo e desenvolvendo seu respectivo arquivo CSS.

---
+++ Assets/JS/Game.js
@@ -0,0 +1,16 @@
+const cartas = [
+
+    {carta: "./Assets/Images/Cards/Anri/Anri.jpg"},
+    {carta: "./Assets/Images/Cards/Bachira/Bachira.jpg"},
+    {carta: "./Assets/Images/Cards/Barou/Barou.jpg"},
+    {carta: "./Assets/Images/Cards/Chigiri/Chigiri.jpg"},
+    {carta: "./Assets/Images/Cards/Ego/Ego.jpg"},
+    {carta: "./Assets/Images/Cards/Gagamaru/Gagamaru.jpg"},
+    {carta: "./Assets/Images/Cards/Isagi/Isagi.jpg"},
+    {carta: "./Assets/Images/Cards/Kunigami/Kunigami.jpg"},
+    {carta: "./Assets/Images/Cards/Nagi/Nagi.jpg"},
+    {carta: "./Assets/Images/Cards/Reo/Reo.jpg"},
+    {carta: "./Assets/Images/Cards/Rin/Rin.jpg"},
+    {carta: "./Assets/Images/Cards/Sae/Sae.jpg"}
+
+];
