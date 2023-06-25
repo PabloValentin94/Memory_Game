3670ce921733e61df4bad997f55e9a48f2a13a54
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-06-25 04:07:38 -0300

    Fazendo novas modificações no arquivo javascript.
    Adicionando uma função que verifica as duas cartas viradas pelo usuário e, se forem iguais, exibe uma mensagem que avisa que um par de cartas foi encontrado; Adicionando uma função que verifica se todos os pares de cartas foram encontrados e, se sim, exibe uma mensagem alertando que o jogo acabou e que será recarregado; e prevenindo pośsíveis bugs ou trapaças.

--- Assets/JS/Game.js
+++ Assets/JS/Game.js
@@ -19,15 +19,15 @@ var cartas_geradas = [];
 
 var quantidade_vezes_carta_gerada = [0,0,0,0,0,0,0,0,0,0,0,0];
 
-var cartas_encontradas = 0;
+var id_primeira_carta_escolhida = "";
+
+var id_segunda_carta_escolhida = "";
 
 var primeira_carta_escolhida = "";
 
 var segunda_carta_escolhida = "";
 
-var id_primeira_carta_escolhida = "";
-
-var id_segunda_carta_escolhida = "";
+var cartas_encontradas = [];
 
 window.onload = () => {
 
@@ -55,6 +55,15 @@ window.onload = () => {
         if(cartas_geradas.length >= 24)
         {
 
+            /*for(var i = 0; i < 24; i++)
+            {
+
+                console.log(cartas_geradas[i]);
+
+            }
+
+            console.log("\n");*/
+
             clearInterval(gerar_cartas_aleatoriamente);
 
         }
@@ -66,32 +75,98 @@ window.onload = () => {
 function revelar_carta(id)
 {
 
-    const carta_selecionada = document.getElementById(id);
+    var condicao = false;
 
-    carta_selecionada.classList.add("reveal_card");
+    if(cartas_encontradas.length > 0)
+    {
 
-    setTimeout(() => {
+        for(var i = 0; i < cartas_encontradas.length; i++)
+        {
+    
+            if(cartas_encontradas[i] == cartas_geradas[id])
+            {
 
-        carta_selecionada.style = "background-image: url('" + cartas_geradas[id] + "')";
+                condicao = true;
 
-    }, 750);
+            }
+    
+        }
+
+    }
+
+    if(condicao == false)
+    {
+
+        const carta_selecionada = document.getElementById(id);
+
+        carta_selecionada.classList.add("reveal_card");
+    
+        setTimeout(() => {
+    
+            carta_selecionada.style = "background-image: url('" + cartas_geradas[id] + "')";
+    
+            if(primeira_carta_escolhida == "" && segunda_carta_escolhida == "")
+            {
+    
+                id_primeira_carta_escolhida = id;
+    
+                primeira_carta_escolhida = cartas_geradas[id_primeira_carta_escolhida];
+    
+                //console.log("Primeira opção: " + primeira_carta_escolhida);
+    
+            }
+    
+            else
+            {
+
+                if(id_primeira_carta_escolhida != id)
+                {
+
+                    id_segunda_carta_escolhida = id;
+    
+                    segunda_carta_escolhida = cartas_geradas[id_segunda_carta_escolhida];
+        
+                    //console.log("Segunda opção: " + segunda_carta_escolhida);
+
+                }
+    
+            }
+    
+            setTimeout(() => {
+    
+                verificar_cartas_escolhidas();
+            
+            }, 500);
+    
+        }, 750);
+
+    }
 
 }
 
 function verificar_cartas_escolhidas()
 {
 
-    /*if(primeira_carta_escolhida == segunda_carta_escolhida)
+    if(primeira_carta_escolhida != "" && segunda_carta_escolhida != "")
     {
 
-        if(primeira_carta_escolhida != "")
+        if(primeira_carta_escolhida == segunda_carta_escolhida)
         {
 
-            const carta_encontrada = primeira_carta_escolhida;
+            const par_encontrado = primeira_carta_escolhida;
+
+            //const par_encontrado = segunda_carta_escolhida;
+
+            cartas_encontradas.push(par_encontrado);
+
+            /*for(var i = 0; i < cartas_encontradas.length; i++)
+            {
+
+                console.log(cartas_encontradas[i]);
+
+            }*/
 
             alert("Encontrou um par de cartas!");
-    
-            cartas_encontradas.push(carta_encontrada);
 
             id_primeira_carta_escolhida = "";
 
@@ -101,16 +176,46 @@ function verificar_cartas_escolhidas()
 
             segunda_carta_escolhida = "";
 
+            verificar_vitoria();
+
+        }
+
+        else if(primeira_carta_escolhida != segunda_carta_escolhida)
+        {
+
+            document.getElementById(id_primeira_carta_escolhida).classList.remove("reveal_card");
+
+            document.getElementById(id_primeira_carta_escolhida).classList.add("hidden_card");
+
+            document.getElementById(id_segunda_carta_escolhida).classList.remove("reveal_card");
+
+            document.getElementById(id_segunda_carta_escolhida).classList.add("hidden_card");
+
+            setTimeout(() => {
+
+                document.getElementById(id_primeira_carta_escolhida).classList.remove("hidden_card");
+
+                document.getElementById(id_primeira_carta_escolhida).style = "background-image: url('./Assets/Images/Card.jpg');";
+
+                document.getElementById(id_segunda_carta_escolhida).classList.remove("hidden_card");
+
+                document.getElementById(id_segunda_carta_escolhida).style = "background-image: url('./Assets/Images/Card.jpg');";
+
+                id_primeira_carta_escolhida = "";
+
+                id_segunda_carta_escolhida = "";
+
+                primeira_carta_escolhida = "";
+
+                segunda_carta_escolhida = "";
+
+            }, 750);
+
         }
 
     }
 
-    else
-    {
 
-        
-
-    }*/
 
 }
 
@@ -120,13 +225,15 @@ function verificar_vitoria()
     if(cartas_encontradas.length == 12)
     {
 
-        //alert("Fim de Jogo! O jogo será reiniciado.");
+        setTimeout(() => {
 
-        alert("Fim de Jogo!");
+            alert("Fim de Jogo! O jogo será reiniciado.");
 
-        document.querySelector(".card").classList.remove("reveal_card");
+            document.querySelector(".card").classList.remove("reveal_card");
+    
+            window.location.reload(true);
 
-        //window.location.reload(true);
+        }, 1000);
 
     }
 
